9f2762e3943b4b654ec6142c8362070135dafd23
danilkalog <pisma4dan4ik@gmail.com>
2026-02-24 14:47:13 +0300

    Для понимания оставил коментарии

--- ru/hse/java2026/SumCalculator.java
+++ ru/hse/java2026/SumCalculator.java
@@ -5,36 +5,36 @@ public class SumCalculator {
         int sum = 0;
 
         for (String s : args) {
-            int i = 0;
+            int i = 0; // будет нашим индексом, которым мы проходимся по строке
 
-            while (i < s.length()) {
-                char c = s.charAt(i);
+            while (i < s.length()) { //проходимся по строке
+                char c = s.charAt(i); // преобразовываем по индексу в строке в символ
 
-                if (!Character.isDigit(c) && c != '+' && c != '-') {
+                if (!Character.isDigit(c) && c != '+' && c != '-') { // если не число и не знак, то пропускаем этот символ
                     i++;
                     continue;
                 }
 
-                int start = i;
+                int start = i; //считаем, что наш символ (либо число, либо знак) является стартовым и за ним может быть ещё числа
 
-                if (c == '+' || c == '-') {
+                if (c == '+' || c == '-') { // если у нас после числа идет знак, то мы сдвигаем на следующий символ
                     i++;
 
-                    if (i >= s.length() || !Character.isDigit(s.charAt(i))) {
+                    if (i >= s.length() || !Character.isDigit(s.charAt(i))) { // если i за пределами строки (например знак стоит "123-" или на этой позиции не цифра, то возращаемся к циклу while
                         continue;
                     }
                 }
 
 
-                while (i < s.length() && Character.isDigit(s.charAt(i))) {
-                    i++;
+                while (i < s.length() && Character.isDigit(s.charAt(i))) { //на этом шаге мы уверены, что у нас будет цифра, поэтому тут i указывает на первую цирфру числа
+                    i++; // пока у нас в строке и текущий символ цифра, двигаем i вперёд
                 }
 
 
-                String number = s.substring(start, i);
+                String number = s.substring(start, i); // "вырезаем" наше число
 
                 try {
-                    sum += Integer.parseInt(number);
+                    sum += Integer.parseInt(number); // складываем к общей сумме
                 } catch (NumberFormatException e) {
 
                 }
